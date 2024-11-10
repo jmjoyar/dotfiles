0203591946445d01211efc0928428f5890341906
Cosmigonón <mafretd@gmail.com>
2024-11-10 16:33:39 -0500

    telescope layout

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -1,6 +1,5 @@
 -- Plugin: Highly extendable fuzzy finder over lists.
 -- https://github.com/nvim-telescope/telescope.nvim
-
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VeryLazy",
@@ -28,9 +27,21 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+		local action_layout = require("telescope.actions.layout")
 
 		telescope.setup({
 			defaults = {
+				initial_mode = "insert",
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						prompt_position = "bottom",
+						mirror = false,
+						preview_cutoff = 0,
+					},
+				},
+				preview = true,
 				path_display = { "truncate" },
 				mappings = {
 					i = {
@@ -44,6 +55,10 @@ return {
 			pickers = {
 				git_files = { show_untracked = true },
 			},
+
+			file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+			grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+			qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 		})
 		telescope.load_extension("fzf")
 		local keymap = vim.keymap
